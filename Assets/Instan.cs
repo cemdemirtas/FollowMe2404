48a7c9bf56f2d77cbeda7a607492f27f0552de61
Cem Demirtas <97852548+cemdemirtas@users.noreply.github.com>
2022-05-09 15:18:46 +0300

    Revert "Update Instan.cs"
    This reverts commit 1085d7eae92e35a0481df5a2fe76b65795482fe0.

--- Assets/Instan.cs
+++ Assets/Instan.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class Instan : MonoBehaviour
 {
+<<<<<<< HEAD
+    public GameObject cube1,cube2,cube3;
+    float RandomPosXLeft, RandomPosXRight, RandomPosXCenter, RandomPosZ, RandomPosX, RandomPosXFLeft;
+    List<float> Xvalues = new List<float>();
+    private void Awake()
+    {
+       
+        RandomPosXLeft = -8.5f;
+        RandomPosXFLeft = -6f;
+        RandomPosXRight = 2;
+        RandomPosXCenter = 0;
+        Xvalues.Add(RandomPosXRight);
+        Xvalues.Add(RandomPosXLeft);
+        Xvalues.Add(RandomPosXCenter);
+=======
     private int lastNumber;
     public GameObject cube1, cube2, cube3;
     float RandomPosXLeft, RandomPosXRight, RandomPosXCenter, RandomPosZ, RandomPosX, RandomPosXFLeft, RandomPosXFRight;
@@ -20,10 +35,46 @@ public class Instan : MonoBehaviour
         Xvalues.Add(RandomPosXLeft);
         Xvalues.Add(RandomPosXCenter);
         Xvalues.Add(RandomPosXLeft);
+>>>>>>> parent of 3fb71b0 (Revert "Revert "Obstacle and cloness have changed"")
         Xvalues.Add(RandomPosXFLeft);
     }
     void Update()
     {
+<<<<<<< HEAD
+        float GenarateZ= Random.RandomRange(3f, 300f);
+
+        if (cube2.transform.position!=cube1.transform.position && cube1.transform.position!=cube2.transform.position)
+        {
+          
+        
+        if (transform.childCount<15)
+        {
+            GameObject InstantiateFollower = (GameObject)Instantiate(cube1, new Vector3(Xvalues[Random.RandomRange(0, 4)], transform.position.y, GenarateZ), Quaternion.identity);
+            InstantiateFollower.transform.name = "c1DenemeClone" + Random.RandomRange(0, 150);
+            InstantiateFollower.transform.parent = transform;
+        }
+        if (transform.childCount<15)
+        {
+            GameObject InstantiateFollower = (GameObject)Instantiate(cube2, new Vector3(Xvalues[Random.RandomRange(0, 4)], transform.position.y, GenarateZ), Quaternion.identity);
+            InstantiateFollower.transform.name = "c2DenemeClone" + Random.RandomRange(0, 150);
+            InstantiateFollower.transform.parent = transform;
+        }
+        if (transform.childCount<15)
+        {
+            GameObject InstantiateFollower = (GameObject)Instantiate(cube3, new Vector3(Xvalues[Random.RandomRange(0, 4)], transform.position.y, GenarateZ), Quaternion.identity);
+            InstantiateFollower.transform.name = "c3DenemeClone" + Random.RandomRange(0, 150);
+            InstantiateFollower.transform.parent = transform;
+        }
+
+        }
+        //InstantiateFollower.transform.SetParent(FollowersParent);
+        
+
+
+
+
+        }
+=======
         float GenarateZ = Random.RandomRange(3f, 300f);
 
 
@@ -63,13 +114,6 @@ public class Instan : MonoBehaviour
 
 
 
+>>>>>>> parent of 3fb71b0 (Revert "Revert "Obstacle and cloness have changed"")
     }
-    //InstantiateFollower.transform.SetParent(FollowersParent);
-
-
-
-
-
-}
-
 
